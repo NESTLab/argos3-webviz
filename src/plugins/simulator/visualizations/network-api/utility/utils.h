1cf95e49543efb08b74881688a11f3825b4036cc
Prajankya <prajankya@gmail.com>
2020-02-23 10:37:18 -0500

    moved extra files to utility

--- src/plugins/simulator/visualizations/network-api/utility/utils.h
+++ src/plugins/simulator/visualizations/network-api/utility/utils.h
@@ -82,11 +82,6 @@ static inline nlohmann::json GetEntitiesAsJSON(argos::CSpace& c_space) {
 /****************************************/
 /****************************************/
 
-template <typename T>
-static inline void EraseFromVector(std::vector<T>& deque, T const& value) {
-  deque.erase(std::remove(begin(deque), end(deque), value), end(deque));
-};
-
 static inline std::vector<std::string> SplitString(
   const std::string& str, const std::string& delims = " ") {
   std::vector<std::string> output;
@@ -106,27 +101,4 @@ static inline std::vector<std::string> SplitString(
   return output;
 }
 
-// uses std::find_first_of rather than string_view::find_first_of
-static inline std::vector<std::string_view> SplitSV(
-  std::string_view strv, std::string_view delims = " ") {
-  std::vector<std::string_view> output;
-  // output.reserve(strv.length() / 4);
-  auto first = strv.begin();
-
-  while (first != strv.end()) {
-    const auto second = std::find_first_of(
-      first, std::cend(strv), std::cbegin(delims), std::cend(delims));
-    // std::cout << first << ", " << second << '\n';
-    if (first != second) {
-      output.emplace_back(strv.substr(
-        std::distance(strv.begin(), first), std::distance(first, second)));
-    }
-
-    if (second == strv.end()) break;
-
-    first = std::next(second);
-  }
-
-  return output;
-}
 #endif
